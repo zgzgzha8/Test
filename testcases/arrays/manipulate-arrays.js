f5a1da9b06469a3b89cb542100d753500238e574
DxD <97294233+zgzgzha8@users.noreply.github.com>
2024-09-04 14:54:33 +0100

    Update manipulate-arrays.js

--- testcases/arrays/manipulate-arrays.js
+++ testcases/arrays/manipulate-arrays.js
@@ -1,37 +1,7 @@
-function handleCodeRun(code) {
-  try {
-    const capturedOutput = [];
-    const originalConsoleLog = console.log;
-    console.log = (...args) => {
-      capturedOutput.push(
-        args.map((arg) => {
-          if (typeof arg === "object" && arg !== null) {
-            return JSON.stringify(arg);
-          }
-          return arg.toString();
-        }).join(" ")
-      );
-      originalConsoleLog(...args);
-    };
-    if (code) {
-      eval(code);
-    }
-    console.log = originalConsoleLog;
-    return capturedOutput.join("\n");
-  } catch (error) {
-    return `${error}`;
-  }
-}
+const regex = /myArray.shift();/
 
-// إضافة السطر إلى الكود المراد تنفيذه
-let code = 'console.log(myArray);';
-const out = handleCodeRun(code);
-
-let isPass = false;
-let msg = '';
-
-if (out === '["b","c","d"]') {
-  isPass = true;
+if (regex.test(code)) {
+    isPass = true
 } else {
-  msg = "Fail!";
-  }
+    msg = "الجواب خاطئ"
+}
