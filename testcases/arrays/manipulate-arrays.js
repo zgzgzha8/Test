b9c56b9dd06debbbbf561157266091f7b53f92e2
DxD <97294233+zgzgzha8@users.noreply.github.com>
2024-09-04 15:04:50 +0100

    Update manipulate-arrays.js

--- testcases/arrays/manipulate-arrays.js
+++ testcases/arrays/manipulate-arrays.js
@@ -1,7 +1,33 @@
-const regex = /myArray.shift();/
+function handleCodeRun(code) {
+  try {
+    const capturedOutput = [];
+    const originalConsoleLog = console.log;
+    console.log = (...args) => {
+      capturedOutput.push(
+        args.map((arg) => {
+          if (typeof arg === "object" && arg !== null) {
+            return JSON.stringify(arg);
+          }
+          return arg.toString();
+        }).join(" "),
+      );
+      originalConsoleLog(...args);
+    };
+    if (code) {
+      eval(code);
+    }
+    console.log = originalConsoleLog;
+    return capturedOutput.join("\n");
+  } catch (error) {
+    return `${error}`;
+  }
+}
 
-if (regex.test(code)) {
-    isPass = true
+code += "\nconsole.log(X);";
+const out = handleCodeRun(code);
+
+if (out === 'a') {
+  isPass = true;
 } else {
-    msg = "الجواب خاطئ"
+  msg = "Fail!";
 }
